2ee744385a51f7e3fc6b671a6d2961adffdff503
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-09-28 10:14:21 +0500

    issue-206: GetDatabaseFilePath_InstanceAndDatabaseFilePathField_PathEqualsToExpected

--- VelocipedeUtils/Shared/tests/VelocipedeUtils.Shared.DbOperations.Tests/DbConnections/Sqlite/SqliteDbConnectionInitializeTests.cs
+++ VelocipedeUtils/Shared/tests/VelocipedeUtils.Shared.DbOperations.Tests/DbConnections/Sqlite/SqliteDbConnectionInitializeTests.cs
@@ -30,5 +30,34 @@ namespace VelocipedeUtils.Shared.DbOperations.Tests.DbConnections.Sqlite
             // Assert.
             resultPath.Should().Be(expectedPath);
         }
+
+        [Theory]
+        [InlineData("Data Source=mydatabase.db;", "mydatabase.db")]
+        [InlineData("Data Source=mydatabase.db;Mode=ReadWriteCreate;Foreign Keys=True;", "mydatabase.db")]
+        [InlineData("Data Source=mydatabase.db;Mode=ReadWriteCreate;Cache=Shared;Foreign Keys=True;", "mydatabase.db")]
+        [InlineData("Data Source='mydatabase.db';", "mydatabase.db")]
+        [InlineData("Data Source='mydatabase.db';Mode=ReadWriteCreate;Foreign Keys=True;", "mydatabase.db")]
+        [InlineData("Data Source='mydatabase.db';Mode=ReadWriteCreate;Cache=Shared;Foreign Keys=True;", "mydatabase.db")]
+        [InlineData("Data Source='relative-path/mydatabase.db';", "relative-path/mydatabase.db")]
+        [InlineData("Data Source='relative-path/mydatabase.db';Mode=ReadWriteCreate;Foreign Keys=True;", "relative-path/mydatabase.db")]
+        [InlineData("Data Source='relative-path/mydatabase.db';Mode=ReadWriteCreate;Cache=Shared;Foreign Keys=True;", "relative-path/mydatabase.db")]
+        public void GetDatabaseFilePath_InstanceAndDatabaseFilePathField_PathEqualsToExpected(string connectionString, string expectedPath)
+        {
+            // Arrange.
+            ICommonDbConnection dbConnection = DbConnectionsCreator.InitializeDbConnection(DatabaseType.SQLite, connectionString);
+            if (dbConnection == null || dbConnection.DatabaseType != DatabaseType.SQLite)
+            {
+                throw new InvalidOperationException("Incorrect type of the created database connection: SQLite expected");
+            }
+            SqliteDbConnection sqliteDbConnection = (SqliteDbConnection)dbConnection;
+
+            // Act.
+            string instancePath = sqliteDbConnection.DatabaseFilePath;
+            string staticPath = SqliteDbConnection.GetDatabaseFilePath(connectionString);
+
+            // Assert.
+            staticPath.Should().Be(expectedPath);
+            staticPath.Should().Be(instancePath);
+        }
     }
 }
