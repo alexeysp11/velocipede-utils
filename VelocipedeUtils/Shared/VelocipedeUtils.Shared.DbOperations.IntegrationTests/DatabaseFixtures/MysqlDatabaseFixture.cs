b2974686d0000d72eb13342a5c77d946d4a7f6d8
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-10-02 11:56:24 +0500

    issue-206: mysql fixture

---
+++ VelocipedeUtils/Shared/VelocipedeUtils.Shared.DbOperations.IntegrationTests/DatabaseFixtures/MysqlDatabaseFixture.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.Common;
+using MySql.Data.MySqlClient;
+using VelocipedeUtils.Shared.DbOperations.Enums;
+using Testcontainers.MySql;
+
+namespace VelocipedeUtils.Shared.DbOperations.IntegrationTests.DatabaseFixtures
+{
+    public sealed class MysqlDatabaseFixture : IDatabaseFixture
+    {
+        private readonly MySqlContainer container =
+            new MySqlBuilder()
+                .Build();
+
+        public string ConnectionString => container.GetConnectionString();
+        public string ContainerId => $"{container.Id}";
+        public DatabaseType DatabaseType => DatabaseType.MSSQL;
+
+        public DbConnection GetDbConnection()
+            => new MySqlConnection(ConnectionString);
+
+        public Task InitializeAsync()
+            => container.StartAsync();
+
+        public Task DisposeAsync()
+            => container.DisposeAsync().AsTask();
+    }
+}
---
+++ VelocipedeUtils/Shared/VelocipedeUtils.Shared.DbOperations.IntegrationTests/DbConnections/Mysql/MysqlDbConnectionInitializeTests.cs
@@ -0,0 +1,12 @@
+﻿using VelocipedeUtils.Shared.DbOperations.IntegrationTests.DatabaseFixtures;
+using VelocipedeUtils.Shared.DbOperations.IntegrationTests.DbConnections.Base;
+
+namespace VelocipedeUtils.Shared.DbOperations.IntegrationTests.DbConnections.Mysql
+{
+    public sealed class MysqlDbConnectionInitializeTests : BaseDbConnectionInitializeTests, IClassFixture<MysqlDatabaseFixture>
+    {
+        public MysqlDbConnectionInitializeTests(MysqlDatabaseFixture fixture) : base(fixture)
+        {
+        }
+    }
+}
