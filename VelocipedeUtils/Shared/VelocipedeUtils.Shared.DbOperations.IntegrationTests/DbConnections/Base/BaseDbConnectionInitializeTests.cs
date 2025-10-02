e6fb8ff4f44f32271faead551b6d7fac7f9813e2
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-10-02 10:19:25 +0500

    issue-206: integration test project

---
+++ VelocipedeUtils/Shared/VelocipedeUtils.Shared.DbOperations.IntegrationTests/DbConnections/Base/BaseDbConnectionInitializeTests.cs
@@ -0,0 +1,16 @@
+﻿using VelocipedeUtils.Shared.DbOperations.Enums;
+
+namespace VelocipedeUtils.Shared.DbOperations.IntegrationTests.DbConnections.Base
+{
+    public abstract class BaseDbConnectionInitializeTests
+    {
+        private DatabaseType _databaseType;
+        protected string _connectionString;
+
+        protected BaseDbConnectionInitializeTests(DatabaseType databaseType)
+        {
+            _databaseType = databaseType;
+            _connectionString = string.Empty;
+        }
+    }
+}
--- VelocipedeUtils/Shared/tests/VelocipedeUtils.Shared.DbOperations.Tests/DbConnections/Postgres/PostgresDbConnectionInitializeTests.cs
+++ VelocipedeUtils/Shared/tests/VelocipedeUtils.Shared.DbOperations.Tests/DbConnections/Postgres/PostgresDbConnectionInitializeTests.cs
@@ -3,18 +3,14 @@ using FluentAssertions;
 using Npgsql;
 using VelocipedeUtils.Shared.DbOperations.DbConnections;
 using VelocipedeUtils.Shared.DbOperations.Enums;
-using VelocipedeUtils.Shared.DbOperations.Tests.DatabaseFixtures;
 using VelocipedeUtils.Shared.DbOperations.Tests.DbConnections.Base;
 
 namespace VelocipedeUtils.Shared.DbOperations.Tests.DbConnections.Postgres
 {
-    public sealed class PostgresDbConnectionInitializeTests : BaseDbConnectionInitializeTests, IClassFixture<PgDatabaseFixture>
+    public sealed class PostgresDbConnectionInitializeTests : BaseDbConnectionInitializeTests
     {
-        private PgDatabaseFixture _fixture;
-
-        public PostgresDbConnectionInitializeTests(PgDatabaseFixture fixture) : base(DatabaseType.PostgreSQL)
+        public PostgresDbConnectionInitializeTests() : base(DatabaseType.PostgreSQL)
         {
-            _fixture = fixture;
             _connectionString = $"Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=mydatabase;";
         }
 
@@ -45,21 +41,6 @@ namespace VelocipedeUtils.Shared.DbOperations.Tests.DbConnections.Postgres
 
             // Assert.
             result.Should().Be(expected);
-        }
-
-        [Fact]
-        public async Task Database_CanRunQuery()
-        {
-            // Arrange.
-            await using NpgsqlConnection connection = new(_fixture.ConnectionString);
-            connection.Open();
-
-            // Act.
-            const int expected = 1;
-            var actual = await connection.QueryFirstAsync<int>("SELECT 1");
-
-            // Assert.
-            actual.Should().Be(expected);
         }
     }
 }
