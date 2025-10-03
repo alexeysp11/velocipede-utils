030ea23b34b222d671656f87020c73f030ec0287
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-10-03 23:22:59 +0500

    issue-206: create db tables (mysql)

--- VelocipedeUtils/Shared/VelocipedeUtils.Shared.DbOperations.IntegrationTests/DbConnections/Mysql/MysqlDbConnectionQueryTests.cs
+++ VelocipedeUtils/Shared/VelocipedeUtils.Shared.DbOperations.IntegrationTests/DbConnections/Mysql/MysqlDbConnectionQueryTests.cs
@@ -6,11 +6,22 @@ namespace VelocipedeUtils.Shared.DbOperations.IntegrationTests.DbConnections.Mys
     public sealed class MysqlDbConnectionQueryTests : BaseDbConnectionQueryTests, IClassFixture<MysqlDatabaseFixture>
     {
         private const string CREATE_DATABASE_SQL = @"
-create table if not exists ""TestModels"" (
-    ""Id"" int primary key,
-    ""Name"" varchar(50),
-    ""AdditionalInfo"" varchar(50)
-);";
+create table if not exists TestModels (
+    Id int primary key,
+    Name varchar(50) NOT NULL,
+    AdditionalInfo varchar(50));
+
+create table if not exists TestCities (
+    Id int primary key,
+    Name varchar(50) NOT NULL);
+
+create table if not exists TestUsers (
+    Id int primary key,
+    Name varchar(50) NOT NULL,
+    Email varchar(50) NOT NULL,
+    CityId int,
+    AdditionalInfo varchar(50) NULL,
+    FOREIGN KEY (CityId) REFERENCES TestCities(Id));";
 
         public MysqlDbConnectionQueryTests(MysqlDatabaseFixture fixture) : base(fixture, CREATE_DATABASE_SQL)
         {
