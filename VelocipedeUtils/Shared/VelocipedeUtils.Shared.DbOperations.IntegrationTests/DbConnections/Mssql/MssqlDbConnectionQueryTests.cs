75d77b90c0756d8fc2f8fb98a00f245449a90554
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-10-03 22:58:42 +0500

    issue-206: create db tables (ms sql)

--- VelocipedeUtils/Shared/VelocipedeUtils.Shared.DbOperations.IntegrationTests/DbConnections/Mssql/MssqlDbConnectionQueryTests.cs
+++ VelocipedeUtils/Shared/VelocipedeUtils.Shared.DbOperations.IntegrationTests/DbConnections/Mssql/MssqlDbConnectionQueryTests.cs
@@ -6,11 +6,22 @@ namespace VelocipedeUtils.Shared.DbOperations.IntegrationTests.DbConnections.Mss
     public sealed class MssqlDbConnectionQueryTests : BaseDbConnectionQueryTests, IClassFixture<MssqlDatabaseFixture>
     {
         private const string CREATE_DATABASE_SQL = @"
-create table if not exists ""TestModels"" (
+create table ""TestModels"" (
     ""Id"" int primary key,
-    ""Name"" varchar(50),
-    ""AdditionalInfo"" varchar(50)
-);";
+    ""Name"" varchar(50) NOT NULL,
+    ""AdditionalInfo"" varchar(50) NULL);
+
+create table ""TestCities"" (
+    ""Id"" int primary key,
+    ""Name"" varchar(50) NOT NULL);
+
+create table ""TestUsers"" (
+    ""Id"" int primary key,
+    ""Name"" varchar(50) NOT NULL,
+    ""Email"" varchar(50) NOT NULL,
+    ""CityId"" int NULL,
+    ""AdditionalInfo"" varchar(50) NULL,
+    FOREIGN KEY (""CityId"") REFERENCES ""TestCities""(""Id""));";
 
         public MssqlDbConnectionQueryTests(MssqlDatabaseFixture fixture) : base(fixture, CREATE_DATABASE_SQL)
         {
