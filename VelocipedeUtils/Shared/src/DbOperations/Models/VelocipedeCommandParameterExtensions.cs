0f734159aacc2efca7889eee27dc24668ec05e38
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-10-19 21:06:16 +0500

    issue-210: ToDapperParameters

---
+++ VelocipedeUtils/Shared/src/DbOperations/Models/VelocipedeCommandParameterExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+
+namespace VelocipedeUtils.Shared.DbOperations.Models
+{
+    public static class VelocipedeCommandParameterExtensions
+    {
+        /// <summary>
+        /// Convert a list of <see cref="VelocipedeCommandParameter"/> to Dapper's <see cref="DynamicParameters"/>.
+        /// </summary>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static DynamicParameters? ToDapperParameters(this List<VelocipedeCommandParameter> parameters)
+        {
+            if (parameters == null || !parameters.Any())
+                return null;
+
+            var result = new DynamicParameters();
+            foreach (var parameter in parameters)
+            {
+                result.Add(
+                    parameter.Name,
+                    parameter.Value,
+                    parameter.DbType,
+                    parameter.Direction,
+                    parameter.Size,
+                    parameter.Precision,
+                    parameter.Scale);
+            }
+            return result;
+        }
+    }
+}
