f7e65f9e53a535371ebe5ae7791e4b011e7ba2d4
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-11-16 21:47:21 +0500

    issue-230: tests sqlite

---
+++ VelocipedeUtils/Shared/tests/VelocipedeUtils.Shared.DbOperations.IntegrationTests/DbConnections/Sqlite/Connecting/SqliteDbExistsTests.cs
@@ -0,0 +1,13 @@
+﻿using VelocipedeUtils.Shared.DbOperations.IntegrationTests.Constants;
+using VelocipedeUtils.Shared.DbOperations.IntegrationTests.DatabaseFixtures;
+using VelocipedeUtils.Shared.DbOperations.IntegrationTests.DbConnections.Base.Connecting;
+
+namespace VelocipedeUtils.Shared.DbOperations.IntegrationTests.DbConnections.Sqlite.Connecting;
+
+public sealed class SqliteDbExistsTests : BaseDbExistsTests, IClassFixture<SqliteDatabaseFixture>
+{
+    public SqliteDbExistsTests(SqliteDatabaseFixture fixture)
+        : base(fixture, SqliteTestConstants.CREATE_DATABASE_SQL)
+    {
+    }
+}
---
+++ VelocipedeUtils/Shared/tests/VelocipedeUtils.Shared.DbOperations.IntegrationTests/DbConnections/Sqlite/Metadata/SqliteGetSqlDefinitionTests.cs
@@ -0,0 +1,13 @@
+﻿using VelocipedeUtils.Shared.DbOperations.IntegrationTests.Constants;
+using VelocipedeUtils.Shared.DbOperations.IntegrationTests.DatabaseFixtures;
+using VelocipedeUtils.Shared.DbOperations.IntegrationTests.DbConnections.Base.Metadata;
+
+namespace VelocipedeUtils.Shared.DbOperations.IntegrationTests.DbConnections.Sqlite.Metadata;
+
+public sealed class SqliteGetSqlDefinitionTests : BaseGetSqlDefinitionTests, IClassFixture<SqliteDatabaseFixture>
+{
+    public SqliteGetSqlDefinitionTests(SqliteDatabaseFixture fixture)
+        : base(fixture, SqliteTestConstants.CREATE_DATABASE_SQL, SqliteTestConstants.CREATE_TESTMODELS_SQL, SqliteTestConstants.CREATE_TESTUSERS_SQL)
+    {
+    }
+}
