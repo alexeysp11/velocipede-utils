5f996695fd2ce1ff93a7e519956a3d31185d6ea9
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-12-05 23:42:59 +0500

    issue-239: VelocipedeDbConnectParamsExceptionTests

--- VelocipedeUtils/Shared/src/DbOperations/Exceptions/VelocipedeDbConnectParamsException.cs
+++ VelocipedeUtils/Shared/src/DbOperations/Exceptions/VelocipedeDbConnectParamsException.cs
@@ -9,12 +9,6 @@ namespace VelocipedeUtils.Shared.DbOperations.Exceptions;
 public class VelocipedeDbConnectParamsException : Exception
 {
     /// <summary>
-    /// Gets the value of <c>InnerException.Message</c>.
-    /// </summary>
-    /// <remarks>If <c>InnerException</c>, or <c>InnerException.Message</c> is null, then <see cref="string.Empty"/> is expected.</remarks>
-    public string InnerExceptionMessage => InnerException?.Message ?? "";
-
-    /// <summary>
     /// Constructor that sets the <see cref="Exception.Message"/> field to default value.
     /// </summary>
     /// <remarks>Default value of the exception is specified as <see cref="ErrorMessageConstants.UnableToConnectToDatabase"/>.</remarks>
@@ -36,7 +30,7 @@ public class VelocipedeDbConnectParamsException : Exception
     /// Constructor that sets the <see cref="Exception.Message"/> field to specified value.
     /// </summary>
     public VelocipedeDbConnectParamsException(string message)
-        : base(message)
+        : base(ExceptionHelper.WrapMessageIfNull(message, typeof(VelocipedeDbConnectParamsException)))
     {
     }
 
@@ -44,7 +38,7 @@ public class VelocipedeDbConnectParamsException : Exception
     /// Constructor that includes info about inner exception, and sets the <see cref="Exception.Message"/> field to specified value.
     /// </summary>
     public VelocipedeDbConnectParamsException(string message, Exception innerException)
-        : base(message, innerException)
+        : base(ExceptionHelper.WrapMessageIfNull(message, typeof(VelocipedeDbConnectParamsException)), innerException)
     {
     }
 }
