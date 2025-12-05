ddcde960041ba83aab9eef2ce45726cf74354c08
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-12-05 23:38:48 +0500

    issue-239: VelocipedeDbCreateExceptionTests

--- VelocipedeUtils/Shared/src/DbOperations/Exceptions/VelocipedeDbCreateException.cs
+++ VelocipedeUtils/Shared/src/DbOperations/Exceptions/VelocipedeDbCreateException.cs
@@ -9,12 +9,6 @@ namespace VelocipedeUtils.Shared.DbOperations.Exceptions;
 public class VelocipedeDbCreateException : Exception
 {
     /// <summary>
-    /// Gets the value of <c>InnerException.Message</c>.
-    /// </summary>
-    /// <remarks>If <c>InnerException</c>, or <c>InnerException.Message</c> is null, then <see cref="string.Empty"/> is expected.</remarks>
-    public string InnerExceptionMessage => InnerException?.Message ?? "";
-
-    /// <summary>
     /// Constructor that sets the <see cref="Exception.Message"/> field to default value.
     /// </summary>
     /// <remarks>Default value of the exception is specified as <see cref="ErrorMessageConstants.UnableToCreateDatabase"/>.</remarks>
@@ -36,7 +30,7 @@ public class VelocipedeDbCreateException : Exception
     /// Constructor that sets the <see cref="Exception.Message"/> field to specified value.
     /// </summary>
     public VelocipedeDbCreateException(string message)
-        : base(message)
+        : base(ExceptionHelper.WrapMessageIfNull(message, typeof(VelocipedeDbCreateException)))
     {
     }
 
@@ -44,7 +38,7 @@ public class VelocipedeDbCreateException : Exception
     /// Constructor that includes info about inner exception, and sets the <see cref="Exception.Message"/> field to specified value.
     /// </summary>
     public VelocipedeDbCreateException(string message, Exception innerException)
-        : base(message, innerException)
+        : base(ExceptionHelper.WrapMessageIfNull(message, typeof(VelocipedeDbCreateException)), innerException)
     {
     }
 }
