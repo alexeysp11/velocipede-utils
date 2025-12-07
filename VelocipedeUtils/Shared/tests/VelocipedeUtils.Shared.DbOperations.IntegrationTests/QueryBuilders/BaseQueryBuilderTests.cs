80f80e14de57225d90a1e3e39a842fd0b84b2720
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-12-07 19:13:09 +0500

    issue-239: BaseQueryBuilderTests
    Integration test description in xml comments

--- VelocipedeUtils/Shared/tests/VelocipedeUtils.Shared.DbOperations.IntegrationTests/QueryBuilders/BaseQueryBuilderTests.cs
+++ VelocipedeUtils/Shared/tests/VelocipedeUtils.Shared.DbOperations.IntegrationTests/QueryBuilders/BaseQueryBuilderTests.cs
@@ -5,6 +5,16 @@ namespace VelocipedeUtils.Shared.DbOperations.IntegrationTests.QueryBuilders;
 /// <summary>
 /// Integration tests for <see cref="ICreateTableQueryBuilder"/>.
 /// </summary>
+/// <remarks>
+/// The integration tests for <see cref="ICreateTableQueryBuilder"/> must validate the correctness of the table creation
+/// in the database.
+/// An approximate validation algorithm should include:
+/// <list type="number">
+/// <item><description>Constructing and executing a query;</description></item>
+/// <item><description>Obtaining metadata about the table;</description></item>
+/// <item><description>Comparing the metadata with the expected collection.</description></item>
+/// </list>
+/// </remarks>
 public abstract class BaseQueryBuilderTests
 {
 }
