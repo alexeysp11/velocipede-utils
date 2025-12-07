130fc4940e6b50eed9186fba28086e80baba80a0
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-12-07 12:41:26 +0500

    issue-239: BuildTests

---
+++ VelocipedeUtils/Shared/tests/VelocipedeUtils.Shared.DbOperations.Tests/QueryBuilders/CreateTableQueryBuilderTests/BuildTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using VelocipedeUtils.Shared.DbOperations.Constants;
+using VelocipedeUtils.Shared.DbOperations.Enums;
+using VelocipedeUtils.Shared.DbOperations.Exceptions;
+using VelocipedeUtils.Shared.DbOperations.QueryBuilders.CreateTableQueryBuilders;
+
+namespace VelocipedeUtils.Shared.DbOperations.Tests.QueryBuilders.CreateTableQueryBuilderTests;
+
+/// <summary>
+/// Unit tests for <see cref="CreateTableQueryBuilder.Build"/>.
+/// </summary>
+public sealed class BuildTests
+{
+    [Theory]
+    [InlineData(DatabaseType.SQLite)]
+    [InlineData(DatabaseType.PostgreSQL)]
+    [InlineData(DatabaseType.MSSQL)]
+    public void BuildOnce_NoException(DatabaseType databaseType)
+    {
+        // Arrange.
+        string tableName = "TableName";
+        CreateTableQueryBuilder createQueryBuilder = new(databaseType, tableName);
+        Func<ICreateTableQueryBuilder> act = createQueryBuilder.Build;
+
+        // Act & Assert.
+        act
+            .Should()
+            .NotThrow();
+    }
+
+    [Theory]
+    [InlineData(DatabaseType.SQLite)]
+    [InlineData(DatabaseType.PostgreSQL)]
+    [InlineData(DatabaseType.MSSQL)]
+    public void BuildTwice_ThrowsVelocipedeQueryBuilderException(DatabaseType databaseType)
+    {
+        // Arrange.
+        string tableName = "TableName";
+        CreateTableQueryBuilder createQueryBuilder = new(databaseType, tableName);
+        createQueryBuilder.Build();
+        Func<ICreateTableQueryBuilder> act = createQueryBuilder.Build;
+
+        // Act & Assert.
+        act
+            .Should()
+            .Throw<VelocipedeQueryBuilderException>()
+            .WithMessage(ErrorMessageConstants.QueryBuilderIsBuilt);
+    }
+}
